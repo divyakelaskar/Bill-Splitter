6bb6f8958aeab5d736023f0973876c36b2caeeba
Divya Kelaskar <divyakelaskar@gmail.com>
2022-01-12 16:01:34 +0000

    ✨ added the bill splitter script

---
+++ bill_splitter.py
@@ -0,0 +1,10 @@
+print("\n💸 Bill Splitter\n")
+
+total_bill = int(input("Enter the bill amount to be paid : "))
+
+no_of_people = int(input("Enter the no of people paying the amount : "))
+
+bill_split_amount = int(total_bill / no_of_people)
+
+print("\nEach person owes : ",bill_split_amount,"\n"50
+)
